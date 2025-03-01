3031566888a63014689304a9ccefd8e82a7abcf5
adarsh singh <adsredmi182615@gmail.com>
2025-03-01 14:24:01 +0530

    added model

--- backend/src/routes/user.route.js
+++ backend/src/routes/user.route.js
@@ -1,7 +1,8 @@
 import { Router } from "express";
+import User from "../model/user.model.js"
 const router = Router()
-router.post('/register',async()=>{
-
+router.post('/register',async(req,res)=>{
+    const {username,password,firstname,lastname} = req.body
 })
 router.post('/login',async()=>{
 
